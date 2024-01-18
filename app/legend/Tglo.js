3999d6aa793a4ae62e93e453cf66dcd392414dcf
sedama <sathwikareddy0799@gmail.com>
2024-01-18 11:40:56 -0600

    changed tglo

--- app/legend/Tglo.js
+++ app/legend/Tglo.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Linking } from 'react-native';
 
-export default function Tglo() {
-  Linking.openURL('https://cgis.glo.texas.gov/Beachwatch/');
+export default function Tglo({ navigation }) {
+  useEffect(() => {
+    const openTgloLink = async () => {
+      await Linking.openURL('https://cgis.glo.texas.gov/Beachwatch/');
+      navigation.goBack();
+    };
+    openTgloLink();
+  }, [navigation]);
+
   return null; // Render nothing
 }
